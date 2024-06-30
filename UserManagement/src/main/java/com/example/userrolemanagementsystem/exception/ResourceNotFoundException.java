edba8f43a2bb1b0ee398f067be9cd558877c215c
danish1657 <64883381+danish1657@users.noreply.github.com>
2024-06-30 16:55:54 +0200

    Delete UserManagement directory

--- UserManagement/src/main/java/com/example/userrolemanagementsystem/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.userrolemanagementsystem.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
