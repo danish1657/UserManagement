d519fb818ed420eefcfadf06daf512a9f9ca84f4
danish1657 <64883381+danish1657@users.noreply.github.com>
2024-07-02 12:24:40 +0200

    Delete UserManagement-backend directory
    uploading latest code with implementation of openAPI

--- UserManagement-backend/src/main/java/com/example/userrolemanagementsystem/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.userrolemanagementsystem.repositories;
-
-import com.example.userrolemanagementsystem.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
